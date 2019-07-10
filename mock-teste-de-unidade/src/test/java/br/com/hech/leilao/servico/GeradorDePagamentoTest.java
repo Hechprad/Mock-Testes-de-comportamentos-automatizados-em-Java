1fb76d8959369feb7b35647dbc66c20064e6980d
Jorge Hech <diordiaum@hotmail.com>
2019-07-10 13:45:33 -0300

    testando domingo

--- mock-teste-de-unidade/src/test/java/br/com/hech/leilao/servico/GeradorDePagamentoTest.java
+++ mock-teste-de-unidade/src/test/java/br/com/hech/leilao/servico/GeradorDePagamentoTest.java
@@ -75,4 +75,35 @@ public class GeradorDePagamentoTest {
 		assertEquals(Calendar.MONDAY, pagamentoGerado.getData().get(Calendar.DAY_OF_WEEK));
 		assertEquals(9, pagamentoGerado.getData().get(Calendar.DAY_OF_MONTH));
 	}
+	
+	@Test
+	public void deveEmpurrarPagamentoNoDomingoParaOProximoDiaUtil() {
+		RepositorioDeLeiloes leiloes = mock(RepositorioDeLeiloes.class);
+		RepositorioDePagamentos pagamentos = mock(RepositorioDePagamentos.class);
+		Relogio relogio = mock(Relogio.class);
+		
+		Leilao leilao = new CriadorDeLeilao()
+				.para("Teclado mecânico")
+				.lance(new Usuario("José da Silva"), 2000.0)
+				.lance(new Usuario("Maria Pereira"), 2500.0)
+				.constroi();
+		
+		when(leiloes.encerrados()).thenReturn(Arrays.asList(leilao));
+		
+		Calendar domingo = Calendar.getInstance();
+		domingo.set(2012, Calendar.APRIL, 8);
+		
+		when(relogio.hoje()).thenReturn(domingo);
+		
+		GeradorDePagamento gerador = new GeradorDePagamento(leiloes, pagamentos, new Avaliador(), relogio);
+		gerador.gera();
+		
+		ArgumentCaptor<Pagamento> argumento = ArgumentCaptor.forClass(Pagamento.class);
+		verify(pagamentos).salva(argumento.capture());
+		
+		Pagamento pagamentoGerado = argumento.getValue();
+		
+		assertEquals(Calendar.MONDAY, pagamentoGerado.getData().get(Calendar.DAY_OF_WEEK));
+		assertEquals(9, pagamentoGerado.getData().get(Calendar.DAY_OF_MONTH));
+	}
 }
