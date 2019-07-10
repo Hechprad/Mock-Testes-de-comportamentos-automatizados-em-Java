e5cab68b4f9a471512e3155784c5acfa330d8ff5
Jorge Hech <diordiaum@hotmail.com>
2019-07-10 12:14:48 -0300

    capturando argumento com mockito utilizando o ArgumentoCaptor

--- mock-teste-de-unidade/src/test/java/br/com/hech/leilao/servico/GeradorDePagamentoTest.java
+++ mock-teste-de-unidade/src/test/java/br/com/hech/leilao/servico/GeradorDePagamentoTest.java
@@ -1,5 +1,46 @@
 package br.com.hech.leilao.servico;
 
+import org.junit.Test;
+import org.mockito.ArgumentCaptor;
+
+import static org.junit.Assert.assertEquals;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
+import static org.mockito.Mockito.verify;
+
+import java.util.Arrays;
+
+import br.com.hech.leilao.builder.CriadorDeLeilao;
+import br.com.hech.leilao.dominio.Leilao;
+import br.com.hech.leilao.dominio.Pagamento;
+import br.com.hech.leilao.dominio.Usuario;
+import br.com.hech.leilao.infra.dao.RepositorioDeLeiloes;
+import br.com.hech.leilao.infra.dao.RepositorioDePagamentos;
+
 public class GeradorDePagamentoTest {
 
+	@Test
+	public void deveGerarPagamentoParaUmLeilaoEncerrado() {
+		RepositorioDeLeiloes leiloes = mock(RepositorioDeLeiloes.class);
+		RepositorioDePagamentos pagamentos = mock(RepositorioDePagamentos.class);
+		Avaliador avaliador = mock(Avaliador.class);
+		
+		Leilao leilao = new CriadorDeLeilao().para("Teclado mecânico")
+				.lance(new Usuario("José da Silva"), 2000.0)
+				.lance(new Usuario("Maria Pereira"), 2500.0)
+				.constroi();
+		
+		when(leiloes.encerrados()).thenReturn(Arrays.asList(leilao));
+		when(avaliador.getMaiorLance()).thenReturn(2500.0);
+		
+		GeradorDePagamento gerador = new GeradorDePagamento(leiloes, pagamentos, avaliador);
+		gerador.gera();
+		
+		ArgumentCaptor<Pagamento> argumento = ArgumentCaptor.forClass(Pagamento.class);
+		verify(pagamentos).salva(argumento.capture());
+		
+		Pagamento pagamentoGerado = argumento.getValue();
+		
+		assertEquals(2500.0, pagamentoGerado.getValor(), 0.00001);
+	}
 }
